48b744af909702a20389054f87be8007ecb81549
Alexander Hirs <hirsalexander@gmail.com>
2016-03-07 16:52:47 +0300

    - configure transaction manager

--- services/src/main/java/by/webproject/hirs/impl/ImplBuyManager.java
+++ services/src/main/java/by/webproject/hirs/impl/ImplBuyManager.java
@@ -7,10 +7,12 @@ import by.webproject.hirs.exception.PersistException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,7 +21,9 @@ import java.util.List;
  */
 
 @Service("buyManager")
-@Transactional(propagation = Propagation.REQUIRED)
+@Transactional(propagation = Propagation.REQUIRED,
+				isolation = Isolation.READ_COMMITTED,
+				readOnly = false)
 public class ImplBuyManager implements BuyManager {
 
 	protected static Logger logger = Logger.getLogger(ImplBuyManager.class);
@@ -27,35 +31,19 @@ public class ImplBuyManager implements BuyManager {
 	@Autowired
 	IBuyDao buyDao;
 
-//	@Override
-//	public Buy getExecutorByName(String name) throws PersistException {
-//		Buy e = null;
-//		e = buyDao.getExecutorByName(name);
-//		return e;
-//	}
-//
-//	@Override
-//	public List<Executor> getExecutorsByPeriod(int first, int second) throws PersistException {
-//		List<Executor> eList = null;
-//		eList = executorDao.getExecutorsByPeriod(first, second);
-//		return eList;
-//	}
-//
-//	@Override
-//	public List<Executor> getActiveExecutors() throws PersistException {
-//		List<Executor> eList = null;
-//		eList = executorDao.getActiveExecutors();
-//		return eList;
-//	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Buy> getAll() throws PersistException {
-		List<Buy> nList = null;
-		nList = buyDao.getAll();
-		return nList;
+		List<Buy> allList = buyDao.getAll();
+		if (allList == null) {
+			allList = new ArrayList<>();
+		}
+		return allList;
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public int getCountBuys() throws PersistException {
 		Integer count = 0;
 		count = buyDao.getCountBuys();
@@ -63,41 +51,49 @@ public class ImplBuyManager implements BuyManager {
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public String[] getBuysListNameArray() throws PersistException {
 		return buyDao.getBuysListNameArray();
 	}
 
 	@Override
+	@Transactional
 	public Buy add(Buy buy) throws PersistException {
 		return buyDao.save(buy);
 	}
 
 	@Override
+	@Transactional
 	public void update(Buy buy) throws PersistException {
 		buyDao.update(buy);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Buy get(Serializable id) throws PersistException {
 		return buyDao.get(id);
 	}
 
 	@Override
+	@Transactional (rollbackFor = Exception.class)
 	public void delete(Buy buy) throws PersistException {
 		buyDao.remove(buy);
 	}
 
 	@Override
+	@Transactional (rollbackFor = Exception.class)
 	public void delete(Serializable id) throws PersistException {
 		buyDao.remove(buyDao.get(id));
 	}
 
 	@Override
+	@Transactional
 	public void refresh(Buy buy) throws PersistException {
 		buyDao.refresh(buy);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Buy> loadAll() throws PersistException {
 		return buyDao.loadAll();
 	}
